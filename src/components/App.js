6c9807a8afc34a8995220c486f423bc55d59b893
gautampgit <gautamk30@gmail.com>
2020-03-10 23:56:55 +0530

    axios call to api

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import axios from 'axios'
 import SearchBar from './SearchBar'
 
-const App = ()=>{
-    return(
-        <div className="ui container">
-            <SearchBar/>
-        </div>
-    );
+class App extends React.Component {
+
+    // constructor() {
+    //     super();
+    // }
+    //5BSTbChbrHOFZfn5PGaNZSqheiFSYMRChsIdT9rNnSo
+
+    handleSearchSubmit = (term) => {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 5BSTbChbrHOFZfn5PGaNZSqheiFSYMRChsIdT9rNnSo'
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="ui container">
+                <SearchBar onSearchSubmit={this.handleSearchSubmit} />
+            </div>
+        );
+    }
 }
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,32 @@
 import React from 'react';
 
-class SearchBar extends React.Component{
+class SearchBar extends React.Component {
 
-    constructor(){
+    constructor() {
         super();
-        this.state={
+        this.state = {
             term: null,
         };
         this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleFormSubmit = this.handleInputChange.bind(this);
+        //this.handleFormSubmit = this.handleInputChange.bind(this);
     }
-    handleFormSubmit(event){
+    handleFormSubmit = (event) => {
         event.preventDefault();
-        console.log(this.state.term);
+        this.props.onSearchSubmit(this.state.term);
     }
 
-    handleInputChange(event){
-       this.setState({term:event.target.value});
+    handleInputChange(event) {
+        this.setState({ term: event.target.value });
     }
-    render(){
-        return(
+    render() {
+        return (
             <div className="ui segment">
-                <form  onSubmit ={this.handleFormSubmit}   className="ui form">
-                    <div className ="field">
+                <form onSubmit={this.handleFormSubmit} className="ui form">
+                    <div className="field">
                         <label>Image Search</label>
-                        <input type = "text" onChange ={this.handleInputChange}/>
-        <p>{this.state.term}</p>
+                        <input type="text" onChange={this.handleInputChange} />
                     </div>
-                    
+
                 </form>
             </div>
         );
