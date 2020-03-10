ff506b847d3407508968cd2065ef407f0a9be34b
gautam <j.gautam1@tcs.com>
2020-03-10 10:41:20 +0530

    startedproject

---
+++ src/components/SearchBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class SearchBar extends React.Component{
+
+    constructor(){
+        super();
+        this.state={
+            term: null,
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmit = this.handleInputChange.bind(this);
+    }
+    handleFormSubmit(event){
+        event.preventDefault();
+        console.log(this.state.term);
+    }
+
+    handleInputChange(event){
+       this.setState({term:event.target.value});
+    }
+    render(){
+        return(
+            <div className="ui segment">
+                <form  onSubmit ={this.handleFormSubmit}   className="ui form">
+                    <div className ="field">
+                        <label>Image Search</label>
+                        <input type = "text" onChange ={this.handleInputChange}/>
+        <p>{this.state.term}</p>
+                    </div>
+                    
+                </form>
+            </div>
+        );
+    }
+}
+export default SearchBar;
